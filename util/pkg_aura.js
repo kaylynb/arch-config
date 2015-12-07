5bd460115e9b82ee6ad5dc6ab135b7687b22d456
Kaylyn Bogle <kaylyn@kaylyn.ink>
2015-12-06 21:23:05 -0800

    updated aura pkg to be faster (use pacman as base and only override install cmd)

--- util/pkg_aura.js
+++ util/pkg_aura.js
@@ -1,18 +1,10 @@
 'use strict'
 
-const Pkg = require('meth/lib/command/pkg/')
+const PkgPacman = require('meth/lib/command/pkg/pacman')
 const exec = require('meth/lib/util/exec')
 
-module.exports = class PkgAura extends Pkg {
-	isInstalled (packageName) {
-		return exec.status(`aura -Ai ${exec.escape(packageName)}`)
-	}
-
+module.exports = class PkgAura extends PkgPacman {
 	install (packageName) {
 		exec.run(`aura -A --noconfirm ${exec.escape(packageName)}`)
-	}
-
-	remove (packageName) {
-		exec.run(`aura -Rs --noconfirm ${exec.escape(packageName)}`)
 	}
 }
