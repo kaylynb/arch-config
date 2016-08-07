79ff51d8449e58a93ec2144c80b26585c68a473a
Kaylyn Bogle <kaylyn@kaylyn.ink>
2016-08-06 17:32:17 -0700

    Added workaround to nvidia metamode issues in latest drivers

--- configs/nvidia/index.js
+++ configs/nvidia/index.js
@@ -5,6 +5,7 @@ const path = require('path')
 module.exports = (m, a, u) => {
 	m.pkg.install('nvidia-libgl')
 	m.pkg.install('nvidia')
+	m.pkg.install('nvidia-settings')
 
 	m.file.install('/etc/X11/xorg.conf.d/20-nvidia.conf', {
 		source: path.resolve(__dirname, '20-nvidia.conf'),
@@ -13,5 +14,12 @@ module.exports = (m, a, u) => {
 			owner: 'root',
 			group: 'root'
 		}
+	})
+
+	// Need to set metamodes at login instead of XORG conf due to bug in nvidia drivers
+	// See: https://devtalk.nvidia.com/default/topic/950647/linux/black-screen-with-367-35/
+	m.file.install(u.home('.xprofile.d/nvidia-metamode-set'), {
+		source: path.resolve(__dirname, 'xprofile.d/nvidia-metamode-set'),
+		permissions: u.mode(0o500)
 	})
 }
