ecce79976562b1c63439038c611dbbcb10c7b802
Kaylyn Bogle <kaylyn@kaylyn.ink>
2016-08-13 09:17:12 -0700

    added directory to rofi config

--- configs/rofi/index.js
+++ configs/rofi/index.js
@@ -5,6 +5,10 @@ const path = require('path')
 module.exports = (m, a, u) => {
 	m.pkg.install('rofi')
 
+	m.directory.install(u.home('.config/rofi'), {
+		permissions: u.mode(0o700)
+	})
+
 	m.file.install(u.home('.config/rofi/config'), {
 		source: path.resolve(__dirname, 'config'),
 		permissions: u.mode(0o600)
