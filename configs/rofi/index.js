1e60552ca380523be2c346da1aa4fcfacfb3a537
Kaylyn Bogle <kaylyn@kaylyn.ink>
2016-08-13 09:30:07 -0700

    added rofi font size config

--- configs/rofi/index.js
+++ configs/rofi/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const path = require('path')
+const tpl = require('../../util/tpl')
 
 module.exports = (m, a, u) => {
 	m.pkg.install('rofi')
@@ -9,8 +10,13 @@ module.exports = (m, a, u) => {
 		permissions: u.mode(0o700)
 	})
 
+	const rofiConfig = tpl.render(
+		path.resolve(__dirname, 'config'),
+		a.rofi
+	)
+
 	m.file.install(u.home('.config/rofi/config'), {
-		source: path.resolve(__dirname, 'config'),
+		content: rofiConfig,
 		permissions: u.mode(0o600)
 	})
 }
