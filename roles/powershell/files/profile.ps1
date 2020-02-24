16f7c16d049563ffaa16e00b022fd32b76614c2d
Kaylyn Bogle <kaylyn@kaylyn.ink>
2020-02-24 15:49:55 -0800

    Add windows configs:
    fontconfig
    powershell
    fzf
    ripgrep
    windows-terminal

--- roles/powershell/files/profile.ps1
+++ roles/powershell/files/profile.ps1
@@ -1,1 +1,16 @@
 Set-PSReadLineOption -EditMode Emacs -BellStyle None
+
+Remove-PSReadlineKeyHandler 'Ctrl+r'
+Remove-PSReadlineKeyHandler 'Ctrl+t'
+Import-Module PSFzf
+
+function prompt {
+	$Host.UI.RawUI.WindowTitle = "$env:USERNAME@$($env:COMPUTERNAME):$PWD"
+
+	$ExitCodeDisplay = ""
+	if ($LASTEXITCODE) {
+		$ExitCodeDisplay = "$LASTEXITCODE "
+	}
+
+	"`e[38;5;3m$((Get-Date).ToLongTimeString())`e[0m `e[38;5;6m[$env:USERNAME@$env:COMPUTERNAME]`e[0m `e[38;5;5m$PWD`e[0m`r`n`e[38;5;1m$($ExitCodeDisplay)`e[0m> "
+}
